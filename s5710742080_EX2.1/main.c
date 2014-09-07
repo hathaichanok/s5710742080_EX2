31c01f59797abb773a8f76f60796b9b94895ad2b
hathaichanok <hathai_milk@hotmail.com>
2014-09-07 10:09:32 +0700

    5710742080_EX2

---
+++ s5710742080_EX2.1/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    printf("Enter Number : ");
+    scanf("%d",&a);
+
+    if(a%2 == 0){
+        printf("a is even");
+    }
+    else {
+        printf("a is odd");
+    }
+}
